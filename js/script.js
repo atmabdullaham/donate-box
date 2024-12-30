e9d3befa224b3087ca33759e3622b4aca9c152e8
atmabdullaham <atmabdullaham@gmail.com>
2024-12-30 19:37:19 +0600

    added the history function shortly

--- js/script.js
+++ js/script.js
@@ -12,6 +12,11 @@ function setInnerText(id, text) {
  document.getElementById(id).innerText = text
 }
 
+// function for remove class
+function removeClass(id, className) {
+ document.getElementById(id).classList.remove(className);
+}
+
 
 function mainTask(id) {
  document.getElementById(id + "-button").addEventListener("click", function (event) {
@@ -25,6 +30,21 @@ function mainTask(id) {
   setInnerText("balance", newBalance)
   setInnerText(id + "-balance", newdonationAmount)
 
+  const historyItem = document.createElement("div");
+  historyItem.className = "bg-white p-3 rounded-md border-l-2 border-indigo-500"
+
+  historyItem.innerHTML = `
+  
+  
+  <p class="text-xs text-gray-700"> ${new Date().toLocaleDateString()} </p>
+  <p class="text-md text-gray-700"> Balance ${newBalance.toFixed(2)} </p>
+  <p class="text-xs text-gray-700"> Expenses ${donationInput.toFixed(2)} </p>
+  <p class="text-xs text-gray-700"> Balance ${newdonationAmount.toFixed(2)} </p>
+  
+ `
+  const historyContainer = document.getElementById("history-list");
+  historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+  removeClass("history-section", "hidden")
  })
 }
 
